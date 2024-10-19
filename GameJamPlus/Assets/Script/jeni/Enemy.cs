95118a6ee5b4fc7259aea27a102d05b6293bb5c1
Jeniffer <115024190+JenifferXavier@users.noreply.github.com>
2024-10-19 14:28:28 -0300

    qwe

---
+++ GameJamPlus/Assets/Script/jeni/Enemy.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject pointA;
+    public GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Transform currentPoint;
+    public float speed;
+    private Transform player;
+
+    public float attackRange;
+    public int attackDamage = 10;  
+    public float attackRate = 1.0f;
+    private float nextAttackTime = 0f;
+
+    //private PlayerMovement playerHealth;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        //playerHealth = player.GetComponent<PlayerMovement>();
+    }
+    void Update()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= attackRange)
+        {
+            Attack();
+        }
+        else
+        {
+            Patrol();
+        }
+    }
+
+    void Patrol()
+    {
+        Vector2 point = currentPoint.position - transform.position;
+        animator.SetBool("Patrolling", true);
+        animator.SetBool("Attacking", false);
+
+        if (currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        }
+        else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            Flip();
+            currentPoint = pointA.transform;
+        }
+
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            Flip();
+            currentPoint = pointB.transform;
+        }
+    }
+
+    private void Flip()
+    {
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    void Attack()
+    {
+        animator.SetBool("Attacking", true);
+        animator.SetBool("Patrolling", false);
+
+        rb.velocity = Vector2.zero;
+
+        //if (Time.time >= nextAttackTime)
+        //{
+            
+          //  if (playerHealth != null)
+         //   {
+          //      playerHealth.PerderVida(attackDamage);
+          //  }
+
+         //   nextAttackTime = Time.time + 1f / attackRate;
+       // }
+    }
+
+}

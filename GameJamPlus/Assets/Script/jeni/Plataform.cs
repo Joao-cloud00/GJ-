a8f823a94910de52830a7af5ba7256a04a291d7f
Jeniffer <115024190+JenifferXavier@users.noreply.github.com>
2024-10-19 22:37:32 -0300

    fix

--- GameJamPlus/Assets/Script/jeni/Plataform.cs
+++ GameJamPlus/Assets/Script/jeni/Plataform.cs
@@ -2,31 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Plataform : MonoBehaviour
+public class Platform : MonoBehaviour
 {
     public GameObject pointA;
-    public GameObject pointB;
-    private Rigidbody2D rb;
-    private Transform currentPoint;
-    public float speed;
+    public GameObject pointB; 
+    private Transform currentPoint; 
+    public float speed = 2f; 
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
         currentPoint = pointB.transform;
     }
 
     void Update()
     {
-        Vector2 point = currentPoint.position - transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, currentPoint.position, speed * Time.deltaTime);
 
-        if (currentPoint == pointB.transform)
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.1f)
         {
-            rb.velocity = new Vector2(speed, 0);
-        }
-        else
-        {
-            rb.velocity = new Vector2(-speed, 0);
+            currentPoint = currentPoint == pointA.transform ? pointB.transform : pointA.transform;
         }
     }
 }
